02c6585a0a29d18d28e15d30b41674c6b422904d
Benjamin Davis <bmd-callisto@pm.me>
2022-09-13 01:59:00 -0400

    fixed sector

--- iv_models/db_sector.go
+++ iv_models/db_sector.go
@@ -1,14 +1,18 @@
 package iv_models
 
 type DB_Sector struct {
-	Name           string                 `json:"name" bson:"name"`
+	Id             DB_Sector_Id           `json:"_id" bson:"_id"`
 	HistoricalData []DB_Sector_Historical `json:"historicalData" bson:"historicalData"`
+}
+
+type DB_Sector_Id struct {
+	Name string `json:"name" bson:"name"`
 }
 
 type DB_Sector_Historical struct {
 	Date        string  `json:"date" bson:"date"`
-	Total       float32 `json:"total" bson:"total"`
-	TotalBought float32 `json:"totalBought" bson:"totalBought"`
-	TotalSold   float32 `json:"totalSold" bson:"totalSold"`
-	BuyOrSell   float32 `json:"buyOrSell" bson:"buyOrSell"`
+	Total       float64 `json:"total" bson:"total"`
+	TotalBought float64 `json:"totalBought" bson:"totalBought"`
+	TotalSold   float64 `json:"totalSold" bson:"totalSold"`
+	BuyOrSell   string  `json:"buyOrSell" bson:"buyOrSell"`
 }
